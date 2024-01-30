4180f5426cc9cecdabcc16205409b7caf482ffad
Garry <tahinjanaharygarry@gmail.com>
2024-01-30 21:20:30 +0300

    setup cors

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from tortoise.contrib.fastapi import register_tortoise
 
 from src.config.app import APP_CONFIG
 from src.config.container import Container
-from src.routers.user import user_router
 from src.routers.account import account_router
 from src.routers.auth import auth_router
+from src.routers.user import user_router
 
 
 def create_app() -> FastAPI:
     container = Container()
 
     api = FastAPI(title="Rish Tanata APP")
+
+    api.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
     api.container = container
     api.include_router(user_router)
     api.include_router(account_router)
