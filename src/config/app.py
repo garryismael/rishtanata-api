479e8ae24a93ce4837370ef4f9b41339cad4c5fe
Garry <tahinjanaharygarry@gmail.com>
2024-01-31 00:14:23 +0300

    setup env

--- src/config/app.py
+++ src/config/app.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class AppConfig(BaseSettings):
-    model_config = SettingsConfigDict(env_file=".env.local", env_file_encoding="utf-8")
+    model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
     db_url: str
     secret_key: str
